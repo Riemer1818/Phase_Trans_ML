b202d07ab5566e1c9c49b140db87c51e251e3f37
Riemer1818 <Riemer.vandervliet@live.nl>
2021-06-11 12:02:11 +0200

    added 2D qsub

---
+++ 2D_qsub/call_2D_10.sh
@@ -0,0 +1,9 @@
+#1: number of temperatures
+#2: number of states saved per temperature last 50%
+#3: states per temperature (how many times we run the similation total)
+#4: grid length
+#5: ferromagnetic (1) antiferromagnetic (-1)
+#6: number of itirations per atom on average
+#7: output directory
+
+julia ~/Phase_Trans_ML/make_data_2D.jl 100 10 500 10 1 25 ./Phase_Trans_ML/train_data
